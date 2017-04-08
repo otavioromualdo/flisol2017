b4efa053c395b74ace580f40c7a75f1e9eb8c03c
Otavio Romualdo Barbosa <otavio_rb2000@hotmail.com>
2017-04-08 16:40:52 -0300

    incluindo teste.c
    Signed-off-by: Otavio Romualdo Barbosa <otavio_rb2000@hotmail.com>

---
+++ teste.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+main(){
+
+	int a = 2;
+	int b = 2;
+
+	int soma = a + b;
+
+	printf("Soma: %d", soma); 
+	prinf("Teste.....");
+	system("pause");
+}
